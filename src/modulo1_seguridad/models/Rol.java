548dbf9fb757541a45039d4d0a619a63e4ba8bc4
mela04-debug <vanemela04@gmail.com>
2026-05-20 09:27:03 -0400

    Modelo Rol para Módulo 1

---
+++ src/modulo1_seguridad/models/Rol.java
@@ -0,0 +1,63 @@
+package modulo1_seguridad.models;
+
+import java.io.Serializable;
+
+/**
+ * Modelo de Rol para autorización
+ * Integrante: 1 - Seguridad y Usuarios
+ */
+public class Rol implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private int idRol;
+    private String nombreRol;
+    
+    /**
+     * Constructor vacío
+     */
+    public Rol() {
+    }
+    
+    /**
+     * Constructor completo
+     */
+    public Rol(int idRol, String nombreRol) {
+        this.idRol = idRol;
+        this.nombreRol = nombreRol;
+    }
+    
+    // ===== GETTERS Y SETTERS =====
+    public int getIdRol() {
+        return idRol;
+    }
+    
+    public void setIdRol(int idRol) {
+        this.idRol = idRol;
+    }
+    
+    public String getNombreRol() {
+        return nombreRol;
+    }
+    
+    public void setNombreRol(String nombreRol) {
+        this.nombreRol = nombreRol;
+    }
+    
+    @Override
+    public String toString() {
+        return nombreRol;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Rol rol = (Rol) obj;
+        return idRol == rol.idRol;
+    }
+    
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(idRol);
+    }
+}
